1e76ee2ca326f728d51c061c331b817dc604dec7
Briarwoodsz <johnjosephhoy@gmail.com>
2019-11-04 14:09:26 -0700

    created insert code for orm
    made insert function to allow us to add to our table, and a question mark function that will take in the number of arguments and return it as a string of question marks to put into our sql bit to allow for multiple things to be inserted.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,15 @@
-let connection = require("../config/connection.js");
+// requires the conection db so the ORM can communicate with the databse
+let connection = require("./connection.js");
 
+// this is to loop through our sql to generate the number of question marks we need for a result
+function questionMarks(num) {
+        let arr = [];
+
+        for (let i = 0; i < num; i++) {
+                arr.push("?");
+        }
+        return arr.toString();
+}
 
 let orm = {
         selectAll: function (tableInput, cb) {
@@ -11,7 +21,27 @@ let orm = {
                         cb(res);
                 });
         },
-        // insertOne:
+        insertOne: function (table, cols, vals, cb) {
+                let queryString = "INSERT INTO " + table;
+
+                queryString += " (";
+                queryString += cols.toString();
+                queryString += ") ";
+                queryString += "VALUES (";
+                queryString += questionMarks(vals.length);
+                queryString += ") ";
+
+                console.log(queryString);
+
+                connection.query(queryString, vals, function (err, result) {
+                        if (err) {
+                                throw err
+                        }
+                        cb(result);
+                })
+        },
+
+        
 
         // updateOne:
 }
