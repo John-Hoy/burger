b82a83b65538b8eee0c544edfb42415ba6360c47
Briarwoodsz <johnjosephhoy@gmail.com>
2019-11-05 01:29:32 -0700

    started server construction
    started to add the dependencies to my server and addded a body parser to my packages to my modules

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-let express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller.js");
+
+
+let app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use("/", routes);
+
+app.listen(port);	
